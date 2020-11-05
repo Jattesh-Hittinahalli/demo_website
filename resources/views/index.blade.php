0d58fc5b4b0d8b52079b78fe8be7898fca6beb31
Jattesh-Hittinahalli <71567266+Jattesh-Hittinahalli@users.noreply.github.com>
2020-11-05 15:09:13 +0530

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,338 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+
+  <title>Coaching institute</title>
+  <link rel="stylesheet" href="/css/style.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.map"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Maven+Pro&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" />
+
+
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+
+    <a class="navbar-brand mynavbar-brand" href="index.html">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="product_display.html">Product Display <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="location.html">See Location <span class="sr-only">(current)</span></a>
+        </li>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="Contactdetails.html">Contact Us <span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+
+    </div>
+  </nav>
+  <!--End Of Navigation -->
+
+  <div id="carouselExampleIndicators" class="carousel slide shadow" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="1.jpg.jpg" class="d-block w-100" width="1146px" height="535px">
+      </div>
+      <div class="carousel-item">
+        <img src="2.jpg.png" class="d-block w-100" width="1146px" height="535px">
+      </div>
+      <div class="carousel-item">
+        <img src="3.jpg.jpg" class="d-block w-100" width="1146px" height="535px">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  <div class="conrainer my-4">
+
+
+    <div class="container">
+
+      <div class="row justify-content-center">
+        <div class="col-md-4">
+          <div class="card " style="width: 20rem;">
+            <div class="inner">
+              <img src="dis.jpg" class="card-img-top" alt="..." width="1146px" height="250px">
+            </div>
+
+            <div class="card-body text-center">
+              <h5 class="card-title animated infinite heartBeat">Company Descripation</h5>
+              <p class="card-text">A company overview (also known as company information or a company summary) is an
+                essential part of a business plan. It's an overview of the most important points about your company—your
+                history, management team, location, mission statement and legal structure.</p>
+              <a href="#" class="btn btn-warning animated infinite heartBeat">More</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-4">
+          <div class="card " style="width: 20rem;">
+            <div class="inner">
+
+              <img src="histo.jpg" class="card-img-top" alt="..." width="1146px" height="250px">
+
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title animated infinite heartBeat">Company History</h5>
+              <p class="card-text">SA company overview (also known as company information or a company summary) is an
+                essential part of a business plan. It's an overview of the most important points about your company—your
+                history, management team, location, mission statement and legal structure.</p>
+              <a href="#" class="btn btn-warning animated infinite heartBeat">More </a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-4">
+          <div class="card" style=" width: 20rem;">
+            <div class="inner">
+              <img src="produ.jpg" class="card-img-top" alt="..." width="1146px" height="250px">
+
+            </div>
+
+            <div class="card-body text-center">
+              <h5 class="card-title animated infinite heartBeat">About Product</h5>
+              <p class="card-text ">A company overview (also known as company information or a company summary) is an
+                essential part of a business plan. It's an overview of the most important points about your company—your
+                history, management team, location, mission statement and legal structure.</p>
+              <a href="#" class="btn btn-warning animated infinite heartBeat">More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="cont">
+          <div class="row">
+            <div class="col-4-lg mr-5">
+              <div class="card" style="width: 20rem;">
+                <div class="inner">
+                  <img src="client.jpg" class="card-img-top" alt="..." width="1146px" height="250px">
+                </div>
+                <div class="card-body text-center">
+                  <h5 class="card-title  animated infinite heartBeat">Our Client's</h5>
+                  <p class="card-text">A company overview (also known as company information or a company summary) is an
+                    essential part of a business plan. It's an overview of the most important points about your
+                    company—your history, management team, location, mission statement and legal structure..</p>
+                  <a href="#" class="btn btn-warning animated infinite heartBeat">More</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-4-lg mr-5">
+              <div class="card " style="width: 20rem;">
+                <div class="inner">
+                  <img src="service.jpg" class="card-img-top" alt="..." width="1146px" height="250px">
+                </div>
+                <div class="card-body text-center">
+                  <h5 class="card-title animated infinite heartBeat">Our Services</h5>
+                  <p class="card-text">A company overview (also known as company information or a company summary) is an
+                    essential part of a business plan. It's an overview of the most important points about your
+                    company—your history, management team, location, mission statement and legal structure.</p>
+                  <a href="#" class="btn btn-warning animated infinite heartBeat">More</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-4-lg mr-5">
+              <div class="card " style="width: 20rem;">
+                <div class="inner">
+                  <img src="team.jpg" class="card-img-top" alt="..." width="1146px" height="250px">
+                </div>
+                <div class="card-body text-center">
+                  <h5 class="card-title animated infinite heartBeat">Our Team</h5>
+                  <p class="card-text">A company overview (also known as company information or a company summary) is an
+                    essential part of a business plan. It's an overview of the most important points about your
+                    company—your history, management team, location, mission statement and legal structure.</p>
+                  <a href="#" class="btn btn-warning animated infinite heartBeat">More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="conrainer my-4">
+    <div class="cardrot">
+      <div class="thecard">
+        <div class="thefront">
+          <svg width="5em" height="5em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+          </svg>
+          <h2>Message From Director</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.
+
+          </p>
+
+          <p1 class="par ">-----By Elon Musk</p1>
+
+
+        </div>
+        <div class="theback">
+          <svg width="5em" height="5em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+          </svg>
+          <h2>Message From CEO</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.
+
+          </p>
+
+          <p1 class="par">-----By Elon Musk</p1>
+
+        </div>
+
+
+
+      </div>
+
+
+
+    </div>
+
+
+    <div class="conrainer my-4">
+
+      <footer class="footer_style">
+        <main class="container-fluid">
+          <div class="row p-3">
+            <div class="col-md-4 col-12 offset-md-1">
+
+
+              <p class="mt-5 text-justify"> My name is Jattesh, Result orianted Web Developer with BE specialized in
+                Computer Science from Visvesvaraya Technological University (VTU), having well Skill in Web developing,
+                Web design, Web Testing, web scraping, sentiment analysis, chat bot, and problem solver in programming.
+                Computer in Web B.E oriented with I am Be Gra Below text is demo text A company overview (also known as
+                company information or a
+                company summary) is an essential part of a business plan. </p>
+              <p class="text-capitalize"> &#169 2020 Jattesh Hittinahalli. All right reserved. </p>
+            </div>
+
+            <div class="col-md-4 col-12">
+              <h4>Contacts</h4>
+              <dl>
+                <dt>Address:</dt>
+                <dd>Pune, Maharashtra </dd>
+              </dl>
+              <dl>
+                <dt>email:</dt>
+                <dd><a href="Jattesh13@gmail.com">Jattesh13@gmail.com</a></dd>
+              </dl>
+              <dl>
+                <dt>phones:</dt>
+                <dd><a href="tel:#">+91 63660456861</a> <span>or</span> <a href="tel:#">+91 6360456861</a>
+                </dd>
+              </dl>
+            </div>
+
+            <div class="col-md-2 col-12">
+              <h4>Links</h4>
+              <ul>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contacts</a></li>
+                <li><a href="#">About</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="d-flex flex-row justify-content-center align-items-center flex-wrap">
+            <div class="p-md-5 p-2 flex-fill animated infinite heartBeat">
+              <a href="https://www.facebook.com/"> <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                <span>Facebook</span> </a> </div>
+
+            <div class="p-md-5 p-2 flex-fill animated infinite heartBeat"><a href="https://www.instagram.com/"><i
+                  class="fa fa-instagram" aria-hidden="true"></i><span>Instagram</span></a></div>
+
+            <div class="p-md-5 p-2 flex-fill animated infinite heartBeat"><a href="#"><i class="fa fa-twitter"
+                  aria-hidden="true"></i><span>Twitter</span></a></div>
+
+            <div class="p-md-5 p-2 flex-fill animated infinite heartBeat"><a href="#"><i class="fa fa-youtube"
+                  aria-hidden="true"></i><span>Youtube</span></a></div>
+
+          </div>
+        </main>
+      </footer>
+
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+</div>
+
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+
+
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
